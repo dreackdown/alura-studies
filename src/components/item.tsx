4e112ef5f975abdddba7ea4345fead712161ed51
dreackdown <hugofaria157@live.com>
2023-07-06 07:09:28 -0300

    correcao css

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -1,36 +1,83 @@
-import style from 'Item.module.scss';
-import {Task} from "../types/task.ts";
+import { Task } from '../types/task.ts'
+import styled from 'styled-components'
 
 interface ItemProps extends Task {
-    selectTask: (selectedTask: Task) => void;
+  selectTask: (selectedTask: Task) => void
 }
+
+const ItemWrapper = styled.li`
+  background-color: #4d4d4d;
+  border-radius: 8px;
+  box-shadow: 2px 4px 4px #0000009f;
+  padding: 12px;
+  margin-bottom: 8px;
+  position: relative;
+  cursor: pointer;
+
+  h3 {
+    margin-bottom: 8px;
+    word-break: break-all;
+  }
+
+  span {
+    color: #d0d0d0;
+  }
+
+  &.selectedItem {
+    background-color: #292929;
+    box-shadow: 2px 4px 4px #0000009f inset;
+  }
+
+  &.completedItem {
+    background-color: #566f42;
+    cursor: auto;
+
+    .completed {
+      display: block;
+      background-image: url('/src/assets/check-mark.svg');
+      background-repeat: no-repeat;
+      background-size: 38px 38px;
+      position: absolute;
+      top: 50%;
+      right: 12px;
+      transform: translateY(-50%);
+      width: 42px;
+      height: 43px;
+    }
+  }
+`
 
 const Item = ({
-                  task,
-                  time,
-                  selected,
-                  completed,
-                  id,
-                  selectTask,
-              }: ItemProps) => {
-    return (
-        <li
-            className={`${style.item} ${selected ? style.itemSelecionado : ''} ${completed ? style.itemCompletado : ''}`}
-            onClick={() => !completed && selectTask(
-                {
-                    task,
-                    time,
-                    selected,
-                    completed,
-                    id
-                }
-            )}
-        >
-            <h3>{task}</h3>
-            <span>{time}</span>
-            {completed && <span className={style.concluido} aria-label="task completed"></span>}
-        </li>
-    )
+  task,
+  time,
+  selected,
+  completed,
+  id,
+  selectTask
+}: ItemProps) => {
+  return (
+    <ItemWrapper
+      className={`${selected ? 'selectedItem' : ''} ${
+        completed ? 'completedItem' : ''
+      }`}
+      onClick={() =>
+        !completed &&
+        selectTask({
+          task,
+          time,
+          selected,
+          completed,
+          id
+        })
+      }
+    >
+      <h3>{task}</h3>
+      <span>{time}</span>
+      {completed && (
+        <span className="completed" aria-label="task completed"></span>
+      )}
+    </ItemWrapper>
+  )
 }
 
-export default Item;
+export default Item
--- src/components/stopwatch.tsx
+++ src/components/stopwatch.tsx
@@ -1,9 +1,9 @@
-import styled from "styled-components";
-import {useEffect, useState} from "react";
-import Watch from "./watch.tsx";
-import {Task} from "../types/task.ts";
-import Button from "./button.tsx";
-import {timeToSeconds} from "../common/time.ts";
+import styled from 'styled-components'
+import { useEffect, useState } from 'react'
+import Watch from './watch.tsx'
+import { Task } from '../types/task.ts'
+import Button from './button.tsx'
+import { timeToSeconds } from '../common/time.ts'
 
 const StopWatchWrapper = styled.div`
   display: flex;
@@ -11,11 +11,11 @@ const StopWatchWrapper = styled.div`
   align-items: center;
   grid-area: stopwatch;
 
-  .WatchWrapper {
+  .watchWrapper {
     align-items: center;
-    background-color: #7687A1;
+    background-color: #7687a1;
     border-radius: 10px;
-    box-shadow: 2px 4px 4px #0000009F;
+    box-shadow: 2px 4px 4px #0000009f;
     box-sizing: border-box;
     display: flex;
     font-size: 5rem;
@@ -32,7 +32,7 @@ const StopWatchWrapper = styled.div`
   button {
     background-color: #88bcd1;
     border-radius: 10px;
-    box-shadow: 2px 4px 4px #0000009F;
+    box-shadow: 2px 4px 4px #0000009f;
     color: #272626;
     cursor: pointer;
     font-size: 1.25rem;
@@ -40,8 +40,8 @@ const StopWatchWrapper = styled.div`
     width: 150px;
 
     &:active {
-      background-color: #7CA6B7;
-      box-shadow: 2px 2px 4px #0000009F inset;
+      background-color: #7ca6b7;
+      box-shadow: 2px 2px 4px #0000009f inset;
       cursor: auto;
     }
   }
@@ -62,41 +62,41 @@ const StopWatchWrapper = styled.div`
       font-size: 2.25rem;
     }
   }
-`;
+`
 
 interface StopWatchProps {
-    selected: Task | undefined;
-    fisishTask: () => void;
+  selected: Task | undefined
+  fisishTask: () => void
 }
 
-const StopWatch = ({selected, fisishTask}: StopWatchProps) => {
-    const [time, setTime] = useState<number>();
+const StopWatch = ({ selected, fisishTask }: StopWatchProps) => {
+  const [time, setTime] = useState<number>()
 
-    useEffect(() => {
-        if (selected?.time) {
-            setTime(timeToSeconds(selected.time));
-        }
-    }, [selected]);
-
-    function regressive(counter = 0) {
-        setTimeout(() => {
-            if (counter > 0) {
-                setTime(counter - 1);
-                return regressive(counter - 1);
-            }
-            fisishTask();
-        }, 1000);
+  useEffect(() => {
+    if (selected?.time) {
+      setTime(timeToSeconds(selected.time))
     }
+  }, [selected])
 
-    return (
-        <StopWatchWrapper>
-            <p className="title">Choose a card and start the Stopwatch</p>
-            <div className="watchWrapper">
-                <Watch time={time}/>
-            </div>
-            <Button onClick={() => regressive(time)}>Start!</Button>
-        </StopWatchWrapper>
-    );
+  function regressive(counter = 0) {
+    setTimeout(() => {
+      if (counter > 0) {
+        setTime(counter - 1)
+        return regressive(counter - 1)
+      }
+      fisishTask()
+    }, 1000)
+  }
+
+  return (
+    <StopWatchWrapper>
+      <p className="title">Choose a card and start the Stopwatch</p>
+      <div className="watchWrapper">
+        <Watch time={time} />
+      </div>
+      <Button onClick={() => regressive(time)}>Start!</Button>
+    </StopWatchWrapper>
+  )
 }
 
-export default StopWatch;
+export default StopWatch
